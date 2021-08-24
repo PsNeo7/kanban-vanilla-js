1fcd027c8d9ea93b91a1c372468ca1ccd56cf82a
psneo7 <prateek.jsr.97@gmail.com>
2021-08-24 20:08:26 +0530

    added some styling

--- index.js
+++ index.js
@@ -3,13 +3,17 @@ add_table_button = document.getElementById("add-table-button")
 tableCounter = 0
 
 add_table_button.addEventListener('click', (e) => {
-    newTablePrompt()
+    // newTableName = prompt("Enter New Table Name")
+    // if (newTableName) {
+    //     assnewMain(newTableName)
+    // }
+    createNewTable()
 })
 
 createNewTable = () => {
     tableName = prompt('Enter New Table Name')
     if (tableName) {
-        addNewTable(tableName)
+        addNewMainTable(tableName)
     }
 }
 
@@ -111,7 +115,22 @@ main_table_area.addEventListener("click", (e) => {
         editTable(e.target.dataset.tableNumber)
         // editTable(e.target.parentNode)
     }
+
+    if (e.target.dataset.type == "delete-table-button") {
+        deleteMainTable(e.target.dataset.tableNumber)
+        // editTable(e.target.parentNode)
+    }
 })
+
+function deleteMainTable(tableID) {
+    // console.log("tableid =", tableID);
+    mainTables.forEach((table, index) => {
+        if (table.id == tableID) {
+            mainTables.splice(index, 1)
+            render()
+        }
+    });
+}
 
 function editTable(tableID) {
     console.log("reaached editTable", tableID);
@@ -127,6 +146,7 @@ function editTable(tableID) {
 }
 
 function delTaskInTable(tableID, taskID) {
+    console.log(tableID, taskID);
     mainTables.forEach(table => {
         if (table.id == tableID) {
             table.tasks.forEach((task, index) => {
@@ -174,11 +194,13 @@ function createTableInnerHTML(table) {
         tasks += `
         <div data-task-index="${index}" data-table-number="${table.id}" draggable="true" class="task">
              <span class="desc">${element.desc}</span>
-             <button data-type="edit-task-button">Edit</button> <button data-type="del-task-button">Delete</button>
+             <span class="options" data-table-number="${table.id}" data-task-index="${index}">
+                <button data-type="edit-task-button">Edit</button> <button data-type="del-task-button">Delete</button>
+             </span>
          </div>`
     })
     table_inner_HTML = `<div class="task-table" data-table-number="${table.id}" ondragstart="drag(event)" touchstart="drag(event)" ondrop="drop(event)" ondragover="allowDrop(event)">
-    <div data-table-number="${table.id}" class="heading"><span>${table.name}</span> <button data-table-number="${table.id}" data-type="edit-table-button">Edit Name</button> <button data-type="add-task-button" data-table-number="${table.id}">+</button></div>
+    <div data-table-number="${table.id}" class="heading"><h1 class="title">${table.name}</h1> <button data-table-number="${table.id}" data-type="edit-table-button">Edit Name</button> <button data-table-number="${table.id}" data-type="delete-table-button">Delete Table</button> <button data-type="add-task-button" data-table-number="${table.id}">+</button></div> 
     ${tasks}
     </div>`
 
